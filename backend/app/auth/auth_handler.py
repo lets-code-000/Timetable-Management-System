663548b26d8d48a5f13a0ab47ad37d410b4c5869
CodeSrushti-3105 <158837536+CodeSrushti-3105@users.noreply.github.com>
2026-02-05 21:26:25 +0530

    make OAuth2 login form-compliant and revoke JWTs on user deletion

--- backend/app/auth/auth_handler.py
+++ backend/app/auth/auth_handler.py
@@ -1,21 +1,26 @@
-# app/auth/auth_handler.py
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
 from app.database import get_db
 from app.crud.user import get_user_by_email, verify_password
 from app.crud.jwt import create_access_token
 
-router = APIRouter(prefix="/auth", tags=["Auth"])
+router = APIRouter()
 
 @router.post("/login")
-def login(email: str, password: str, session: Session = Depends(get_db)):
-    user = get_user_by_email(session, email)
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_db)
+):
+    user = get_user_by_email(session, form_data.username)
 
-    if not user or not verify_password(password, user.hashed_password):
+    if not user or not verify_password(
+        form_data.password, user.hashed_password
+    ):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     access_token = create_access_token(
-        data={
+        {
             "sub": user.email,
             "token_version": user.token_version
         }
