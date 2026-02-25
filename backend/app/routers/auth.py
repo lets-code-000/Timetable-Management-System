f062864679fac9b560dcc9e18da513a3636353bd
apekshagangurde <apekshagangurde08@gmail.com>
2026-02-25 11:41:59 +0530

    remove college id from register api

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -2,10 +2,14 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 from app.database import get_db
-from app.schemas.user import UserCreate
-from app.crud.user import get_user_by_username, create_user, verify_password ,get_user_by_email
+from app.schemas.user import UserCreate, UserOut
+from app.crud.user import (
+    get_user_by_username,
+    create_user,
+    verify_password,
+    get_user_by_email,
+)
 from app.crud.jwt import create_access_token
-from app.crud.role import get_role_by_id, create_role
 
 
 from fastapi.security import OAuth2PasswordRequestForm
@@ -13,7 +17,8 @@ from fastapi import Depends
 
 router = APIRouter()
 
-@router.post("/register")
+
+@router.post("/register", response_model=UserOut)
 def register(user: UserCreate, session: Session = Depends(get_db)):
     db_user = get_user_by_username(session, user.username)
     if db_user:
@@ -21,31 +26,20 @@ def register(user: UserCreate, session: Session = Depends(get_db)):
     if get_user_by_email(session, user.email):
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    return create_user(session, user, role_id=None, college_id=user.college_id)
+    return create_user(session, user)
 
-    
 
 @router.post("/login")
 def login(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_db)
+    form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_db)
 ):
     db_user = get_user_by_email(session, form_data.username)
 
-    if not db_user or not verify_password(
-        form_data.password, db_user.hashed_password
-    ):
+    if not db_user or not verify_password(form_data.password, db_user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     access_token = create_access_token(
-        {
-            "sub": db_user.email,
-            "token_version": db_user.token_version
-        }
+        {"sub": db_user.email, "token_version": db_user.token_version}
     )
 
-    return {
-        "access_token": access_token,
-        "token_type": "bearer"
-    }
-
+    return {"access_token": access_token, "token_type": "bearer"}
--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -2,26 +2,28 @@
 from pydantic import BaseModel, EmailStr, validator
 import re
 
+
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     phone_number: str | None
     password: str
-    role_id: int | None = None
-    college_id: int | None = None
 
     @validator("email")
     def validate_domain(cls, v):
         pattern = r"^[a-zA-Z0-9._%+-]+@(gmail\.com|[a-zA-Z0-9-]+\.(com|in|org|net))$"
         if not re.match(pattern, v):
-            raise ValueError("❌ Invalid email. Only Gmail or .com, .in, .org, .net domains are allowed")
+            raise ValueError(
+                "❌ Invalid email. Only Gmail or .com, .in, .org, .net domains are allowed"
+            )
         return v
+
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
-   
-    
+
+
 class UserUpdate(BaseModel):
     username: str | None = None
     email: EmailStr | None = None
@@ -30,17 +32,17 @@ class UserUpdate(BaseModel):
     role_id: int | None = None
     college_id: int | None = None
 
+
 class UserOut(BaseModel):
     id: int
     username: str
     email: EmailStr
     phone_number: str | None
-    role_id: int | None = None
-    college_id: int | None = None
 
     class Config:
-         from_attributes = True
-        
+        from_attributes = True
+
+
 class DeleteUserResponse(BaseModel):
     message: str
     data: UserOut | None = None
